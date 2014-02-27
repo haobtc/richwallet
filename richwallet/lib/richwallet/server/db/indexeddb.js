90df95d118190bae12e4d759c28bafb31eff470e
Zeng Ke <superisaac.ke@gmail.com>
2014-02-27 17:23:32 +0800

    Added missing file

---
+++ richwallet/lib/richwallet/server/db/indexeddb.js
@@ -0,0 +1,135 @@
+var indexeddb = require('indexeddb-js');
+var sqlite3 = require('sqlite3');
+var DB = require('../db');
+var config = require('../config');
+
+function handleCallback(trans, opts, callback) {
+    if(typeof opts == 'function') {
+	callback = opts;
+	opts = {};
+    }
+    trans.onsuccess = function(event) {
+	var obj = ((opts.successResult != undefined)?
+		   opts.successResult:event.target.result);
+	callback(undefined, obj);
+    };
+    trans.onerror = function(event) {
+	callback(event.target.error, opts.errorResult);
+    };
+}
+
+DB.prototype = {
+    connect: function(){
+	var self = this;
+	var engine = new sqlite3.Database(config.backends.indexeddb.sqlite3Path);
+	var indexedDB = new indexeddb.indexedDB('sqlite3', engine);
+	this.request = indexedDB.open('richwallet');
+	this.request.onerror = function(err) {
+	    console.error(err);
+	};
+
+	this.request.onupgradeneeded = function(event) {
+	    self.db = event.target.result;
+	    var store = self.db.createObjectStore('wallet',
+					     {keyPath: 'serverKey'});
+	    store.createIndex('email', 'email', {unique: false});
+	};
+	this.request.onsuccess = function(event) {
+	    self.db = event.target.result;
+	};
+    },
+    getStore: function(mode) {
+	mode = mode || 'readonly';
+	return this.db.transaction(null, mode).objectStore('wallet');
+    },
+    getWalletRecord: function(serverKey, callback) {
+	var store = this.getStore();
+	handleCallback(store.get(serverKey), callback);
+    },
+
+    setAuthKey: function(serverKey, authKey, callback) {
+	var self = this;
+	this.getWalletRecord(serverKey, function(err, record) {
+	    if(err)
+		return callback(err);
+	    var store = self.getStore('readwrite');
+	    record.authKey = authKey;
+	    handleCallback(record.put(record), {
+		successResult: true
+	    }, callback);
+	});
+    },
+    disableAuthKey: function(serverKey, callback) {
+	var self = this;
+	this.getWalletRecord(serverKey, function(err, record) {
+	    if(err)
+		return callback(err);
+	    var store = self.getStore('readwrite');
+	    delete record['authKey'];
+	    handleCallback(record.put(record), {
+		successResult: true
+	    }, callback);
+	});
+    },
+
+    getWallet: function(serverKey, callback) {
+	this.getWalletRecord(serverKey, function(err, payload) {
+	    if(err) {
+		return callback(err);
+	    }
+	    if(!payload) {
+		return callback(undefined, null);
+	    }
+	    callback(undefined, payload.wallet);
+	});
+    },
+    
+    set: function(serverKey, payload, callback) {
+	var store = this.getStore('readwrite');
+	var getter = store.get(serverKey);
+	getter.onsuccess = function(event) {
+	    var origPayload = event.target.result;
+	    payload.serverKey = serverKey;
+	    if(!origPayload || origPayload.payloadHash == payload.originalPayloadHash) {
+		if(payload.newPayloadHash) {
+		    payload.payloadHash = payload.newPayloadHash;
+		}
+		handleCallback(store.put(payload), {
+		    successResult: {wallet:payload.wallet}
+		}, callback);
+	    } else if(origPayload){
+		return callback('out of sync', {wallet: origPayload.wallet});
+	    } else {
+		console.error('szxxcxcx');
+	    }
+	};
+	getter.onerror = function(event) {
+	    callback(event.target.error);
+	};
+    },
+    delete: function(serverKey, callback) {
+	var store = this.getStore('readwrite');
+	handleCallback(store.delete(serverKey), {
+	    successResult: true
+	}, callback);
+    },
+    checkEmailExists: function(email, callback) {
+	var store = this.getStore('readonly');
+	var index = store.index('email');
+	var cursor = index.openCursor(email);
+	cursor.onsuccess = function(event) {
+	    var cursor = event.target.result;
+	    if(!cursor) {
+		callback(undefined, false);
+		return;
+	    } else {
+		callback(undefined, true);
+	    }
+	};
+	cursor.onerror = function(event){
+	  callback(event.target.error);
+	};
+    },
+};
+
+module.exports = DB;
