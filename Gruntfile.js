844711e248654af86d728d0f8bc654abd0425940
Zeng Ke <superisaac.ke@gmail.com>
2014-04-18 18:39:27 +0800

    signup page translate

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,7 +22,7 @@ module.exports = function(grunt) {
     uglify: {
       richwallet: {
         options: {
-          "beautify": true,
+          "beautify": false,
           "screw-ie8": true
         },
         files: {
--- public/js/richwallet.js
+++ public/js/richwallet.js
@@ -12,8 +12,14 @@ richwallet.trans = {
     "propaganda.intro": "One Wallet is a free online crypto currencies wallet which you can use to make worldwide payments for free. We are not a bank, you retain complete ownership of your Money. We cannot view your balance, see your transactions or make payments on your behalf.",
     "intro.secure": "Your wallet  is encrypted  within your browser, before it is saved on our servers, so not even we have access to your account!",
     "intro.onewallet": "BTC, LTC, DOGECOIN and any other cryptcoins  all in one wallet",
-    "intro.open": "Richwallet is an <a href=\"https://github.com/haobtc/richwallet\" target=\"_blank\">open source service</a>, please feel free to use it and contrib to it. Richwallet also benifits from other open source project <a href=\"https://github.com/kyledrake/coinpunk\" target=\"\">coinpunk</a> thanks to open source community."
+    "intro.open": "Richwallet is an <a href=\"https://github.com/haobtc/richwallet\" target=\"_blank\">open source service</a>, please feel free to use it and contrib to it. Richwallet also benifits from other open source project <a href=\"https://github.com/kyledrake/coinpunk\" target=\"\">coinpunk</a> thanks to open source community.",
+
+    "signup.alert": ("<h3><font color=\"red\">IMPORTANT</font> password information:</h3>" +
+		     "<p>Your data is being encrypted in a way that the server cannot spend your coin wallet money. If you lose your password, there is no way to reset it.</p>" +
+		     "<h4><strong>Losing your password is like losing your real-life wallet. You will lose your bitcoins, <u>forever</u></strong>.<br/><u>Remember your password</u>.</h4>" +
+		     "<p>Your password must also be a minimum of <b>10 characters</b>. If you're making a new password, write it down somewhere. You will likely forget it if you don't.</p>")
 };
+
 var language = navigator.language.toLowerCase();
 if(supportedLangs[language]) {
     $.ajax('lang/' + language + '.json', {
